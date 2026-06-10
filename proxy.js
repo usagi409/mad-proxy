9823ddf785e3e2219139986cbb2122ad1f3aebc6
usagi409 <122772801+usagi409@users.noreply.github.com>
2026-06-10 22:43:31 +0900

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -2,16 +2,11 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
 module.exports = (req, res) => {
-    // yt-dlpからのプロキシ要求をYouTubeへ正しく中継する
     proxy.web(req, res, { 
         target: 'https://www.youtube.com',
         changeOrigin: true,
-        // ヘッダーを適切に引き継ぐための設定
-        headers: {
-            'host': 'www.youtube.com'
-        }
+        headers: { 'host': 'www.youtube.com' }
     }, (err) => {
-        console.error('Proxy Error:', err);
-        res.status(502).send('Bad Gateway');
+        res.status(502).send('Proxy Failed');
     });
 };
