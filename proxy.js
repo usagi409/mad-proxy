bcbdfe4e2fb9eb612ac72b0d476b873d512c76cf
usagi409 <122772801+usagi409@users.noreply.github.com>
2026-06-10 22:16:51 +0900

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,10 +1,14 @@
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
-// YouTubeへの通信を中継する
 module.exports = (req, res) => {
+    // YouTubeへの転送設定
     proxy.web(req, res, { 
         target: 'https://www.youtube.com',
-        changeOrigin: true 
+        changeOrigin: true
+    }, (err) => {
+        if (err) {
+            res.status(500).send('Proxy Error');
+        }
     });
 };
